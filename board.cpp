106f6c2b80436857d08fd0e7c06598fafb3890b8
Eli Moser <ecmoser@gmail.com>
2026-02-13 12:40:08 -0500

    checking for check

--- board.cpp
+++ board.cpp
@@ -1,17 +1,55 @@
 #include "board.h"
+#include "pieces.h"
 #include <vector>
 
-Board::Board(std::vector<Piece> pieces) : pieces(pieces) {}
+Board::Board(std::vector<Piece> whitePieces, std::vector<Piece> blackPieces)
+    : whitePieces(whitePieces), blackPieces(blackPieces) {}
 
-std::vector<Piece> Board::getPieces() { return pieces; }
+std::vector<Piece> Board::getWhitePieces() { return whitePieces; }
+std::vector<Piece> Board::getBlackPieces() { return blackPieces; }
 
-Piece::Piece() : position({0, 0}), isWhite(1) {}
-Piece::Piece(std::vector<int> position, bool isWhite)
-    : position(position), isWhite(isWhite) {}
+bool Board::whiteInCheck(std::vector<Piece> whitePieces,
+                         std::vector<Piece> blackPieces) {
+  std::vector<int> whiteKingPos;
+  for (Piece piece : whitePieces) {
+    if (piece.getType() == PieceType::KING) {
+      whiteKingPos = piece.getPosition();
+    }
+  }
+  for (Piece piece : blackPieces) {
+    for (std::vector<int> move : piece.getMoves(*this)) {
+      if (move == whiteKingPos) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+bool Board::blackInCheck(std::vector<Piece> whitePieces,
+                         std::vector<Piece> blackPieces) {
+  std::vector<int> blackKingPos;
+  for (Piece piece : blackPieces) {
+    if (piece.getType() == PieceType::KING) {
+      blackKingPos = piece.getPosition();
+    }
+  }
+  for (Piece piece : whitePieces) {
+    for (std::vector<int> move : piece.getMoves(*this)) {
+      if (move == blackKingPos) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+Piece::Piece() : position({0, 0}), white(1) {}
+Piece::Piece(std::vector<int> position, bool white)
+    : position(position), white(white) {}
 
 std::vector<int> Piece::getPosition() { return position; }
-std::vector<std::vector<int>> Piece::getMoves(const Board &board) {
-  return {{}};
-}
+std::vector<std::vector<int>> Piece::getMoves(Board &board) { return {{}}; }
+
 PieceType Piece::getType() { return PieceType::NONE; }
-bool Piece::getWhite() { return isWhite; }
+bool Piece::isWhite() { return white; }
--- board.h
+++ board.h
@@ -8,23 +8,29 @@ enum class PieceType { KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN, NONE };
 
 class Board {
 public:
-  Board(std::vector<Piece> pieces);
-  std::vector<Piece> getPieces();
+  Board(std::vector<Piece> whitePieces, std::vector<Piece> blackPieces);
+  std::vector<Piece> getWhitePieces();
+  std::vector<Piece> getBlackPieces();
+  bool whiteInCheck(std::vector<Piece> whitePieces,
+                    std::vector<Piece> blackPieces);
+  bool blackInCheck(std::vector<Piece> whitePieces,
+                    std::vector<Piece> blackPieces);
 
 private:
-  std::vector<Piece> pieces;
+  std::vector<Piece> whitePieces;
+  std::vector<Piece> blackPieces;
 };
 
 class Piece {
 public:
   Piece();
   Piece(std::vector<int> position, bool isWhite);
-  virtual std::vector<int> getPosition();
-  virtual std::vector<std::vector<int>> getMoves(const Board &board);
+  virtual std::vector<std::vector<int>> getMoves(Board &board);
   virtual PieceType getType();
-  virtual bool getWhite();
+  std::vector<int> getPosition();
+  bool isWhite();
 
 private:
   std::vector<int> position;
-  const bool isWhite;
+  const bool white;
 };
